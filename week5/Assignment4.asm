af666aecdf89393da7ed234e31caf3786d812977
WeiChien Hsu <weichien711@gmail.com>
2018-07-25 01:14:21 -0500

    :construction: Add assignment4

---
+++ week5/Assignment4.asm
@@ -0,0 +1,45 @@
+TITLE Program Assignment4 -  Composite Numbers Printer  (Assignment3.asm)
+
+; Author: Wei-Chien Hsu
+; Last Modified: 07/19/18
+; OSU email address: hsuweic@oregonstate.edu
+; Course number/section: CS 271 Summer 2018
+; Assignment Number: Programming Assignment #3  Due Date: 07/24/18
+; Get a user request in the range 10 to 200 and generate request random integers
+; in range 100 to 999, stroing them in consecutive elements of an array.
+; Display the list of integers "before sorting, 10 numbers per line."
+; Sort the list in descending order and Calculate and display the medium value, 
+; rounded to the nearest integer. 
+; Display the sorted list, 10 numbers per line.
+; Requirements: 
+; 1.
+; 2. 
+; 3. 
+; 4.
+; 5. 
+; 6. 
+; 7. 
+; 8. 
+; 9.
+; 10.
+; Extra Credit 1 :: Display the numbers ordered by column instead of by row.
+; Extra Credit 2 :: Use a recursive sorting algorithm
+; Extra Credit 3 :: Implement the program using floating-point numbers and the floating-point processor
+; Extra Credit 4 :: Generate the numbers into a file; then read the file into the array.
+
+INCLUDE Irvine32.inc
+
+
+.data
+
+
+.code
+main PROC
+  ; Procedures
+
+	exit	; exit to operating system
+main ENDP
+
+
+
+END main
