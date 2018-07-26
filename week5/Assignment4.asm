7426689d2c2ca47ecc4410329373081b47b880f4
WeiChien Hsu <weichien711@gmail.com>
2018-07-25 22:11:38 -0500

    :construction: Finished Introduction and GetUserData Procedures

--- week5/Assignment4.asm
+++ week5/Assignment4.asm
@@ -51,8 +51,6 @@ INCLUDE Irvine32.inc
   LO_RANGE = 100 
 
 .data
-
-
 ; Introduction
 intro_1   BYTE  " -- Sorting Random Numbers -- ", 0
 intro_2   BYTE  " -- Programmed by Wei-Chien Hus -- ", 0
@@ -60,12 +58,12 @@ intro_3   BYTE  "This program generates a list of random numbers from 100 to 999
 intro_4   BYTE  "displays the original list, sorted list, and calculates the median.", 0
 intor_EC1  BYTE  " Extra Credit :: Use a recursive sorting algorithm ", 0
 intor_EC2  BYTE  " Extra Credit :: Implement the program using floating-point numbers and the floating-point processor ", 0
-intor_EC1  BYTE  " Extra Credit :: Created procedures to change text color and continue until user quits  ", 0
+intor_EC3  BYTE  " Extra Credit :: Created procedures to change text color and continue until user quits  ", 0
 
 ; User Instruction
-instruct_1    BYTE  "Now, we're going to create a list of random numbers, between 10 and 200, ", 0
-prompt        BYTE  "Please ENTER the total number of random digits you would like to generate: ", 0
-error_msg     BYTE  "Input number is out of the RANGE [10 - 200], please ENTER again: ", 0
+instruct_1    BYTE  " Now, we're going to create a list of random numbers. ", 0
+prompt        BYTE  " Please ENTER the total number of random digits between [10 and 200]: ", 0
+error_msg     BYTE  " Input number is out of the RANGE. ", 0
 
 ; Titles
 sortedTitle     BYTE  ":: Sorted Array : ", 0
@@ -74,7 +72,7 @@ medianTitle     BYTE  ":: Median Number : ", 0
 
 ; Array
 random_arr    DWORD  200 DUP(?)
-arrSize       DWORD  ?
+userInput     DWORD  ?
 
 ; EXIT message
 good_bye      BYTE  "Results credited by Wei-Chien Hsu. Goodbye! Enjoy coding.", 0
@@ -82,7 +80,24 @@ again_mes     BYTE  "Want to play again? ", 0
 
 .code
 main PROC
-  ; Procedures
+  ; Intorduction
+  push  OFFSET intro_1    ; [ebp + 36]
+  push  OFFSET intro_2    ; [ebp + 32]
+  push  OFFSET intro_3    ; [ebp + 28]
+  push  OFFSET intro_4    ; [ebp + 24]
+  push  OFFSET intor_EC1  ; [ebp + 20]
+  push  OFFSET intor_EC2  ; [ebp + 16]
+  push  OFFSET intor_EC3  ; [ebp + 12]
+  push  OFFSET instruct_1 ; [ebp + 8]
+  call Introduction
+; Repeat:
+  ; GetData
+  push  OFFSET  error_msg ; [ebp + 16]
+  push  OFFSET  prompt    ; [ebp + 12]
+  push  OFFSET  userInput ; [ebp + 8]
+  call GetData
+
+
 
 	exit	; exit to operating system
 main ENDP
@@ -100,6 +115,8 @@ ENDM
 Introduction PROC
 ; Displays program name, my name, program description and extra credits.
 ; Pre conditions: progName, author, and instruct shoud be defined.
+; Receives: Those strings at [ebp + 8] [ebp + 12] [ebp + 16] [ebp + 20]
+; [ebp + 24] [ebp + 28] [ebp + 32] [ebp + 36]
 ; Returns: Nothing
 ;--------------------------------------------------------------------
   ;; Set Stack Frame and Push Registers
@@ -107,7 +124,28 @@ Introduction PROC
   mov   ebp, esp
   push  edx
 
-
+  call SetTextGrey
+  PrintString [ebp + 36] ; intro_1
+  call CrLf
+  PrintString [ebp + 32] ; intro_2 
+  call CrLf  
+  PrintString [ebp + 28] ; intro_3
+  call CrLf
+  PrintString [ebp + 24] ; EC_1
+  call CrLf
+  PrintString [ebp + 20] ; EC_2
+  call CrLf
+  PrintString [ebp + 16] ; EC_2
+  call CrLf  
+  call CrLf
+  PrintString [ebp + 8] ; instruct_1
+  call CrLf
+  call CrLf
+  
+  pop    edx
+  pop    ebp
+  
+  ret 32
 Introduction ENDP
 
 
@@ -116,14 +154,40 @@ Introduction ENDP
 GetData PROC
 ; Get abd Store user's valid input around 10 to 200.
 ; Pre conditions: Push address of user input into the stack before called
+; Receives: Address of UserInput variable [ebp + 8] (need to be updated), strings of
+; error message [ebp + 16] and strings of prompt to user [ebp + 12].
 ; Returns: User input Stroed in the variable UserInput already
 ;--------------------------------------------------------------------
-  ;; Set Stack Frame and Push Registers
+  ; Set Stack Frame and Push Registers
   push  ebp
   mov   ebp, esp
   push  edx
   push  eax
 
+AskForUser:
+  call SetTextWhite
+  printString [ebp + 12] ; prompt
+  call readInt           ; Read User input and store into EAX Register
+  call CrLf
+  cmp eax, MAX_INPUT ; Data Validation
+  jg   Invalid
+  cmp eax, MIN_INPUT
+  jge  Valid
+
+Invalid:
+  call SetTextRed
+  printString [ebp + 16] ; error message
+  call CrLf
+  jmp  AskForUser
+
+Valid:
+  mov   edx, [ebp + 8] ; Store the address of userInput into edx Register
+  mov   [edx], eax     ; Save the User Input into the address of UserInput Variable by dereference
+
+  pop eax
+  pop edx
+  pop ebp
+  ret 12
 GetData ENDP
 
 
@@ -136,17 +200,17 @@ FillArray PROC
 ;--------------------------------------------------------------------
 
   ;; Set Stack Frame and Push Registers
-    push      ebp
-    mov       ebp, esp
-    push      eax
-    push      ebx
-    push      ecx
-    push      edx
-    push      esi
-    push      edi
+    ; push      ebp
+    ; mov       ebp, esp
+    ; push      eax
+    ; push      ebx
+    ; push      ecx
+    ; push      edx
+    ; push      esi
+    ; push      edi
 
 
-
+  ret
 FillArray ENDP
 
 
@@ -156,17 +220,16 @@ SortList PROC
 ; Pre conditions: push zero, filled array and UserInpur decreasement by 1. 
 ; Returns: A sorted array begins with the largest number.
 ;--------------------------------------------------------------------
-
+  ret
 SortList ENDP
 
 
 ;--------------------------------------------------------------------
 QuickSortHelper PROC
 ; To sort an array in descending order
-; pre conditions:
 ; receiveds: Array's OFFSET [ebp + 8] and size or Array at [ebp + 12]
 ; registers changed:
-; return:
+; return: No return value. But the array has been sorted at this moment.
 ;--------------------------------------------------------------------
 
   ret
@@ -180,6 +243,7 @@ DisplayMedian PROC
 ; Pre conditions: push User Input and Sorted Array.
 ;--------------------------------------------------------------------
 
+  ret
 DisplayMedian ENDP
 
 ;--------------------------------------------------------------------
@@ -225,7 +289,7 @@ SetTextGreen ENDP
 
 ;--------------------------------------------------------------------
 SetTextGrey PROC
-; Make the text green, prvided by Irvine-32 Library
+; Make the text grey, prvided by Irvine-32 Library
 ;--------------------------------------------------------------------
   push      eax
   mov       eax, 7
@@ -237,7 +301,7 @@ SetTextGrey ENDP
 
 ;--------------------------------------------------------------------
 SetTextPurple PROC
-; Make the text green, prvided by Irvine-32 Library
+; Make the text purple, prvided by Irvine-32 Library
 ;--------------------------------------------------------------------
   push      eax
   mov       eax, 13
@@ -246,5 +310,29 @@ SetTextPurple PROC
 
   ret
 SetTextPurple ENDP
+
+;--------------------------------------------------------------------
+SetTextRed PROC
+; Make the text red, prvided by Irvine-32 Library
+;--------------------------------------------------------------------
+  push      eax
+  mov       eax, 12
+  call      setTextColor
+  pop       eax
+
+  ret
+SetTextRed ENDP
+
+;--------------------------------------------------------------------
+SetTextWhite PROC
+; Make the text normal, prvided by Irvine-32 Library
+;--------------------------------------------------------------------
+  push      eax
+  mov       eax, 15
+  call      setTextColor
+  pop       eax
+
+  ret
+SetTextWhite ENDP
 
 END main
