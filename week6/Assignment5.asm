98c179040234818b479a69bdf94b16f265faf730
WeiChien Hsu <weichien711@gmail.com>
2018-08-05 21:19:59 -0500

    :book: format

--- week6/Assignment5.asm
+++ week6/Assignment5.asm
@@ -83,25 +83,25 @@ ENDM
 
 .data
 
-intro_1           BYTE  "", 0
-intro_2           BYTE  "", 0
-intro_3           BYTE  "", 0
-intro_4           BYTE  "", 0
-intro_EC          BYTE  "", 0
-intro_EC          BYTE  "", 0
+intro_1           BYTE  "- Design low level I/O procedures -", 0
+intro_2           BYTE  "- Programed by Wei-Chien Hsu -", 0
+intro_3           BYTE  "Please provide 10 unsigned decimal integers which is fit in 32 bit register.", 0
+intro_4           BYTE  "The Program will display the integers, their sum and average value. ", 0
+intro_EC          BYTE  "**Extra Credit 1:: number each line of user input and display a running subtotal of the user’s numbers.", 0
+intro_EC          BYTE  "**Extra Credit 3:: make your ReadVal and WriteVal procedures recursive.", 0
 
-prompt            BYTE
-error_message     BYTE
+prompt            BYTE  "Please ENTER an unsigned integer: ", 0
+error_message     BYTE  "The number you ENTERED was not an unsigned integer or it was too large.", 0
 
-comma_sing        BYTE  "", 0
-goodbye           BYTE  "", 0
-UserInput_message BYTE  "", 0
-Subtotal_message  BYTE  "", 0
-Sum_message       BYTE  "", 0
-Average_message   BYTE  "", 0
+comma_sing        BYTE  ",", 0
+goodbye           BYTE  "Results credited by Wei-Chien Hsu. goodbye, enjoy coding! ", 0
+UserInput_message BYTE  "The number you ENTERED is: ", 0
+Subtotal_message  BYTE  "The running subtotal is: ", 0
+Sum_message       BYTE  "The total number you ENTERED is: ", 0
+Average_message   BYTE  "The average number is: ", 0
 
-UserInput         BYTE
-CurrentNumber     BYTE
+UserInput         BYTE  MAX_STRING DUP(?)
+CurrentNumber     BYTE  MAX_STRING DUP(?)
 ArraySize         SDWORD  10
 count             DWORD 1
 arr             SDWORD  10(DUP) ?
@@ -109,12 +109,93 @@ Subtotal_number   SDWORD  0
 
 
 .code
+;-------------------------------------------------------------------
+; Main Procedure
+;-------------------------------------------------------------------
+main PROC
 
-  main PROC
+main ENDP
 
-  main ENDP
+;-------------------------------------------------------------------
+; Introduction Procedure
+; Description: Print the program introduction and instruction.
+; Receives: Strings for introduction and instruction.
+; Returns: None.
+; Registers Changed: None.
+;-------------------------------------------------------------------
+Introduction PROC
+
+Introduction ENDP
+
+;-------------------------------------------------------------------
+; ReadVal Procedure
+; Description: Recevie the user input to validate and transfer to decimal value.
+; Receives: Prompt for user input, user input, array, Array Size and error message.
+; Returns: None
+; Registers Changed: eax, ecx, edx, ebx
+;-------------------------------------------------------------------
+ReadVal PROC
+
+ReadVal ENDP
+
+;------------------------------------------------------------------
+; StroeInput Procedure
+; Description: Process the value storing into the Array
+; Receives: UserInput, array
+; Returns:  None
+; Registers Changed: eax, edi
+;------------------------------------------------------------------
+StoreInput PROC
+
+StoreInput ENDP
 
 
+;------------------------------------------------------------------
+; WriteVal Procedure (Recursive)
+; Description: Call DisplayString
+; Receives: @array, number of elements in the array
+; Returns: None
+; Registers Changed: eax, ebx, ecx, edx
+;------------------------------------------------------------------
+WriteVal PROC
+
+WriteVal ENDP
+
+
+;----------------------------------------------------------
+; DisplaySumAndAvg Procedure
+; Description: Calculates the average number and sum of the elements in the array and display them.
+; Receives: UserInput, Subtotal and Sum messsage, array and current 
+; Returns: None
+; Registers Changed: eax, ebx, ecx, edx, edi and ebp
+;----------------------------------------------------------
+DisplaySumAndAvg PROC
+
+DisplaySumAndAvg ENDP
+
+
+;---------------------------------------------
+; ConvertIntToString Procedure
+; Description: Convert Userinput into String
+; Receives: Array, current
+; Returns: None
+; Registers Changed: eax, ebx, ecx, edi and edp
+;---------------------------------------------
+ConvertIntToString PROC
+
+ConvertIntToString ENDP
+
+
+;---------------------------------------------
+; Farewell Procedure
+; Description: Sya goodbye
+; Receives: String of goodbye
+; Returns: None
+; Registers Changed: edx
+;---------------------------------------------
+Farewell PROC
+
+Farewell ENDP
 
 
 END main
